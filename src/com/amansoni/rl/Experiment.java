a6f54e1d59066f680ff9034349cb2ab580c7cedb
sonia2 <sonia2@aston.ac.uk>
2016-05-16 15:34:16 +0100

    Initial RPSO commit

--- src/com/amansoni/rl/Experiment.java
+++ src/com/amansoni/rl/Experiment.java
@@ -6,6 +6,7 @@ import java.util.Random;
  * Created by Aman on 14/05/2016.
  */
 public class Experiment {
+    static int seed = 5;
     Environment environment;
     LearningAlgorithm learningAlgorithm;
 
@@ -15,26 +16,51 @@ public class Experiment {
     }
 
     public static void main(String[] args) {
-        createExperimentRun(30, 100, 1000);
-        createExperimentRun(30, 15, 1000);
+        createExperimentRunRL(30, 100, 2000);
+        createExperimentRunRL(30, 15, 2000);
+
+        createExperimentRunEA(30, 100, 2000);
+        createExperimentRunEA(30, 15, 2000);
+
     }
 
-    public static void createExperimentRun(int repeat, int bias, int steps) {
-        Random random = new Random(1);
+    public static void createExperimentRunRL(int repeat, int bias, int steps) {
+        Random random = new Random(seed);
         System.out.println("Running experiment with bias: " + bias + " repeated " + steps + " averaged over " + repeat);
         long BEGIN = System.currentTimeMillis();
         double rewards = 0;
-        for (int i=0; i< repeat; i++){
-            rewards += createExperiment(bias, steps, random.nextInt());
+        for (int i = 0; i < repeat; i++) {
+            rewards += createExperimentRL(bias, steps, random.nextInt());
         }
-        System.out.println("Averaged reward: " + rewards/ (double)repeat);
+        System.out.println("Averaged reward: " + rewards / (double) repeat);
         long END = System.currentTimeMillis();
         System.out.println("Time: " + (END - BEGIN) / 1000.0 + " sec.");
     }
 
-    public static double createExperiment(int bias, int steps, int seed) {
+    public static double createExperimentRL(int bias, int steps, int seed) {
         Experiment experiment = new Experiment(seed, bias);
         experiment.learningAlgorithm.learn(steps);
         return experiment.learningAlgorithm.getAccumulatedReward();
     }
+
+    public static void createExperimentRunEA(int repeat, int bias, int steps) {
+        Random random = new Random(seed);
+        System.out.println("Running experiment with bias: " + bias + " repeated " + steps + " averaged over " + repeat);
+        long BEGIN = System.currentTimeMillis();
+        double rewards = 0;
+        for (int i = 0; i < repeat; i++) {
+            rewards += createExperimentEA(bias, steps, random.nextInt());
+        }
+        System.out.println("Averaged reward: " + rewards / (double) repeat);
+        long END = System.currentTimeMillis();
+        System.out.println("Time: " + (END - BEGIN) / 1000.0 + " sec.");
+    }
+
+    public static double createExperimentEA(int bias, int steps, int seed) {
+        Experiment experiment = new Experiment(seed, bias);
+        experiment.learningAlgorithm.learn(steps);
+        //experiment.learningAlgorithm.learn(steps, seed, bias);
+        return experiment.learningAlgorithm.getAccumulatedReward();
+    }
+
 }
--- src/com/amansoni/rl/RPSO.java
+++ src/com/amansoni/rl/RPSO.java
@@ -1,21 +1,60 @@
 package com.amansoni.rl;
 
+import com.amansoni.ga.Individual;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Aman on 15/05/2016.
  */
 public class RPSO {
-    private static int SWARM_SIZE =10;
-    private static double ATTRACTION =2.05;
+    final static boolean DEBUG = false;
+    DecimalFormat df = new DecimalFormat("#.00");
+    Environment environment;
+    Environment simulator;
+    Random random;
+    int noOfStates = 21;
+    int noOfActions = 21;
+    int stateOffset = 10;
+    int accumulatedReward = 0;
+    ArrayList<Individual> population;
+
+    private static int SWARM_SIZE = 10;
+    private static double ATTRACTION = 2.05;
     private static double CONSTRICTION_FACTOR = 0.729844;
     private static int VMAX = 20;
-    private static int evaulations_allowed = 100;
+    private static int MAX_EVALUATION_COUNT = 100;
+    private int evaluations = 0;
 
-    public RPSO(){
+    public RPSO(Environment environment, int seed, Environment simulator) {
+        random = new Random(seed);
+        this.environment = environment;
+        this.simulator = simulator;
+        init();
+    }
+
+    private void init() {
+        population = new ArrayList<>();
+        for (int i = 0; i < SWARM_SIZE; i++) {
+            population.add(new Individual(random));
+        }
+    }
+
+    public void learn() {
 
     }
 
-    public void learn(){
+    class Individual {
+        int action = 0;
 
+        public Individual(Random random){
+            action = random.nextInt(noOfActions) - stateOffset;
+        }
+
+        public int evaluateFitness(){
+            return simulator.takeAction(new Action(action));
+        }
     }
-
 }
